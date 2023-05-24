586047d84caa4ff41b89170ab154c1530f29a275
james arasim <jarasim@loandepot.com>
2023-05-24 16:21:35 -0700

    run chrome headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package tests;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.WebDriver;
@@ -20,7 +21,9 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Enable headless mode
+        driver = new ChromeDriver(options);
     }
 
     @AfterMethod
